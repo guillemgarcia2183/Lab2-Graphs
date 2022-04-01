72552fb4115f7ff59c6aeabdd9fdd9decb15a0c2
Pumpunkio <102738127+Pumpunkio@users.noreply.github.com>
2022-04-01 09:52:22 +0200

    Update lab2.py

--- lab2.py
+++ lab2.py
@@ -81,3 +81,55 @@ print("Edges with weight 0:", list1)
 print("Edges with no 0:", list2)
 subax1 = plt.subplot(111)
 nx.draw(G2, with_labels=True, font_weight='bold')
+
+
+
+
+
+
+
+"""
+===============================================
+    TASK 2 -> THE LUCKY DRAW
+===============================================
+
+Tetrahedron: 4-sided dice
+Cube: 6-sided dice
+Octahedron: 8-sided dice
+Dodecahedron: 12-sided dice
+Icosahedron: 20-sided dice
+
+for n throws:
+    
+Tetrahedron throw: maximum value, n*4; minimum value, n; number of different numbers: n*4-n-1
+Cube throw: maximum value, n*6; minimum value, n; number of different numbers: n*6-n-1
+Octahedron throw: maximum value, n*8; minimum value, n; number of different numbers: n*8-n-1
+Dodecahedron throw: maximum value, n*12; minimum value, n; number of different numbers: n*12-n-1
+Icosahedron throw: maximum value, n*20; minimum value, n; number of different numbers: n*20-n-1
+"""
+
+
+def lucky_draw(n):
+    """
+    INPUT
+    n : number of throws a player has.
+    ----------
+    OUTPUT
+    ----------
+    list[list]: a list of lists where every element of the main list is n, ···, n+9 
+    throws of one dice
+    """
+
+    # Initialize a list of lists. It has 5 lists inside for each dice possible
+    final_list = [[] for x in range(5)] 
+    
+    # Lists of the possible results that can be obtained. The first element of each list 
+    # belongs to n throws, the second one to n+1 throws, until n+9 throws.
+    for m in range(n, n+9):
+        final_list[0].append(m*4-m-1)
+        final_list[1].append(m*6-m-1)
+        final_list[2].append(m*8-m-1)
+        final_list[3].append(m*12-m-1)
+        final_list[4].append(m*20-m-1)
+
+    return final_list
