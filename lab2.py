030698fb9b5da0fed422297a9de921f2da109232
Guillem Garcia <102737703+guillemgarcia2183@users.noreply.github.com>
2022-04-07 09:23:00 +0200

    Update lab2.py

--- lab2.py
+++ lab2.py
@@ -15,77 +15,44 @@ Created on Thu Mar 31 08:14:43 2022
 import networkx as nx
 import random as r
 import matplotlib.pyplot as plt
+import examples as ex
 
 def monitor_interests(G):
-    list_weight_0 = []
-    list_not_0 = []
-    
-    edges = G.edges.data()
-    for values in edges:
-        weight = values[2]
-        if weight["weight"] == 0:
-            nodes = (values[0], values[1])
-            list_weight_0.append(nodes)
-        else:
-            nodes = (values[0], values[1])
-            list_not_0.append(nodes)
-    
-    for x in list_weight_0:
-        G.remove_edge(x[0],x[1])
-    
-    return G,list_weight_0,list_not_0
-
-
-#EXAMPLE OF MONITOR_INTERESTS
-def example():
-    G = nx.Graph() #We construct a new graph
-    list_fighters = [] 
-    list_nodes = []
-    file = open("Smash fighters.txt", "r")#Open a file with 80 fighters (Interests)
-    for line in file:
-        list_fighters.append(line[:-1]) #Append into a list of fighters
-    for x in range(10):
-        list_nodes.append(x) #Add each node in a list of nodes
-        auxiliar_list = []
-        counter = 0
-        while counter < 4: #Iterate for the number of interests we want
-            fighter = r.choice(list_fighters) #We choose a random fighter
-            if fighter not in auxiliar_list: #We remove the repited fighters
-                auxiliar_list.append(fighter) 
-                counter += 1 
-        G.add_node(x, topInterests=auxiliar_list) #We add the node with his interests
-        
-    for x in range(10):
+    nodes_of_G = G.nodes()
+    edges_graph = G.edges()
+    new_graph = nx.Graph()
+    new_graph.add_nodes_from(nodes_of_G)
+    for i,edge in enumerate(edges_graph):
         weight = 0
-        random_node = r.choice(list_nodes) #Choose a random node of the list of nodes
-        print("Node:", x)
-        print("Node2:", random_node)
-        interest_x = G.nodes[x]["topInterests"] #See the interests of first node
-        print("Interests node:", interest_x )
-        interest_random = G.nodes[random_node]["topInterests"] #See the interest of second node
-        print("Interests node2:", interest_random )
-        if x != random_node: #If it's not the same node both (We don't want loops)
-            for y in interest_x: 
-                if y in interest_random:
-                    weight += 1 #If interests are the same we increment the weight
-            print("Weight:", weight)
-            print("\n")
-            G.add_edge(x,random_node, weight = weight)
-    return G
+        print("In node" + " " + str(i) + ":")
+        interest_1 = G.nodes[edge[0]]["topInterests"]
+        interest_2 = G.nodes[edge[1]]["topInterests"]
+        print("Interest1:", interest_1)
+        print("Interest2:", interest_2)
 
-G = example() #The example graph we didn't remove 0-weight
-subax1 = plt.subplot(111)
-nx.draw(G, with_labels=True, font_weight='bold')
-G2, list1, list2 = monitor_interests(G)
-print("Edges with weight 0:", list1)
-print("Edges with no 0:", list2)
+        if edge[0] != edge[1]:
+            for x in interest_1:
+                if x in interest_2:
+                    weight += 1
+
+            if weight != 0:
+                    new_graph.add_edge(edge[0],edge[1], weight = weight)
+        
+        print("Weight:", weight)
+        print("")
+        
+    #print("Initial edges:\n", edges_graph)
+    #print("")
+    #print("Final edges:\n", new_graph.edges())   
+    return new_graph
+
+#EXAMPLE PRINCIPAL PROGRAM OF TASK 1
+"""
+G = ex.example_monitor_interest() 
+G2 = monitor_interests(G)
 subax1 = plt.subplot(111)
 nx.draw(G2, with_labels=True, font_weight='bold')
-
-
-
-
-
+"""
 
 """
 ===============================================
